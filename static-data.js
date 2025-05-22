bf2ada7b7378ad1485ab816b68b493b823b2cb47
Ahmad Wakeel <syedahmadshah018@gmail.com>
2025-05-22 05:15:11 +0500

    Static Data and global css fIles

---
+++ static-data.js
@@ -0,0 +1,38 @@
+export const PizzaCardsData = [
+  {
+    name: "Pizza Napoletana special",
+    description:
+      "Born in Napoli, la pizza Napoletana is one of the most famous types of Italian pizza.",
+    image: "/assets/feature-card-1.webp",
+  },
+  {
+    name: "Pizza Napoletana special",
+    description:
+      "Born in Napoli, la pizza Napoletana is one of the most famous types of Italian pizza.",
+    image: "/assets/feature-card-1.webp",
+  },
+  {
+    name: "Pizza Napoletana special",
+    description:
+      "Born in Napoli, la pizza Napoletana is one of the most famous types of Italian pizza.",
+    image: "/assets/feature-card-1.webp",
+  },
+  {
+    name: "Pizza Napoletana special",
+    description:
+      "Born in Napoli, la pizza Napoletana is one of the most famous types of Italian pizza.",
+    image: "/assets/feature-card-1.webp",
+  },
+  {
+    name: "Pizza Napoletana special",
+    description:
+      "Born in Napoli, la pizza Napoletana is one of the most famous types of Italian pizza.",
+    image: "/assets/feature-card-1.webp",
+  },
+  {
+    name: "Pizza Napoletana special",
+    description:
+      "Born in Napoli, la pizza Napoletana is one of the most famous types of Italian pizza.",
+    image: "/assets/feature-card-1.webp",
+  },
+];
